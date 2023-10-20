f3582916a62b11cfa74b5781a16767b057665449
helenaraimundo91 <139161478+helenaraimundo91@users.noreply.github.com>
2023-10-20 18:58:55 +0000

    Project DomainNameGenerator Finished

--- src/app.js
+++ src/app.js
@@ -8,4 +8,21 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ["The", "Our", "we"];
+  let adj = ["amazing", "bad", "good"];
+  let noun = ["architect", "singer", "doctor"];
+  let dominion = [".com", ".pt"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let d = 0; d < dominion.length; d++) {
+          console.log(pronoun[p], adj[a], noun[n], dominion[d]);
+        }
+      }
+    }
+  }
 };
+
+
